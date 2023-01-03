2364476b11b7539ba738d27c646579b2976fc7d6
TranDinhKhoi2002 <87485108+TranDinhKhoi2002@users.noreply.github.com>
2023-01-03 15:05:17 +0700

    config files

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,31 @@
-import '../styles/globals.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { CacheProvider } from '@emotion/react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import createEmotionCache from '../utility/createEmotionCache';
+import darkTheme from '../styles/theme/darkTheme';
+import '../styles/globals.css';
+
+const clientSideEmotionCache = createEmotionCache();
+
+const MyApp = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
+
+export default MyApp;
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
+};
